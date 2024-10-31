9e78729d6d98733d513ef77d58690eb740e81716
Heitor Batista <hbatista@goahead.artico.com>
2024-10-31 11:25:34 -0300

    Meu primeiro projeto

---
+++ app.py
@@ -0,0 +1,35 @@
+import os
+
+print(""""
+
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
+""")
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair\n')
+
+opcao_escolhida = int(input('Escolha uma opção: '))
+
+def finalizar_app():
+    os.system('cls')
+    #quando tiver no mac ---- os.system('clear')
+    print('Finalizando o app\n')
+
+if opcao_escolhida == 1:
+    print('Cadastrar restaurante')
+elif opcao_escolhida == 2:
+    print('Listar restaurantes')
+elif opcao_escolhida == 3:
+    print('Listar restaurante')    
+else:
+    finalizar_app()
+
+
+
