b18b3c60fd86f7d28c1799278424f976857019f8
Heitor Batista <hbatista@goahead.artico.com>
2024-11-01 17:40:26 -0300

    atualização do projeto

---
+++ aula.py
@@ -0,0 +1,39 @@
+import os
+
+def finalizar():
+    # Limpa a tela de acordo com o sistema operacional
+    os.system("cls")
+    print("Obrigado pela informação.")
+
+def par_impares(numero):
+    if numero % 2 == 0:
+        return "Par"
+    else:
+        return "Ímpar"
+
+numero = int(input("Digite um número, por favor: "))
+finalizar()
+print(f"O número que você informou é: {numero}, e ele é {par_impares(numero)}.")
+
+def classificar_idade(idade):
+    if idade <= 12:
+        return "criança"
+    elif idade <= 18:
+        return "adolescente"
+    else:
+        finalizar()
+        return "adulto"
+
+idade = int(input("Digite sua idade: "))
+print(f"Conforme a idade informada ({idade} anos), você é considerado: {classificar_idade(idade)}.")
+
+usario_correto = ("Alura","alura")
+senha_correta = "Alura123"
+
+usuario = input("Digite o nome do usuário: ").lower()
+senha = input("Digite sua senha: ")
+
+if usuario in usario_correto and senha == senha_correta:
+    print("******Login bem sucessido******")
+else:
+    print("Dados inválidos. Tente novamente !!!")
